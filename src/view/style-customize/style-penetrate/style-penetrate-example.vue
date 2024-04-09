9fad89afa35bb92590d019358a58a56ae2e7f3be
lhf <392018756@qq.com>
2024-04-09 23:05:20 +0800

    (+)样式穿透示例增加

--- src/view/style-customize/style-penetrate/style-penetrate-example.vue
+++ src/view/style-customize/style-penetrate/style-penetrate-example.vue
@@ -7,37 +7,36 @@
       这时候我们这么写样式是无法生效的,所以就需要用到样式穿透, 将[data-v-98cf4d84]选择器去除,同时只对当前组件生效, 避免影响其他组件。
     </p>
     <p>
-      当lang="css" 或者不写时，使用<B>&gt;&gt;&gt;</B>
+      当lang="css" 或者不写时，使用 <B>&gt;&gt;&gt;</B>
     </p>
     <el-button @click="showWay1">查看示例（css）</el-button>
     <p>
-      当lang="sass" 或者"less"时，使用<B>/deep/</B>或者<B>::v-deep</B>
+      当 lang="css" 或者不写时，使用 <B>/deep/</B>
     </p>
-    <p style="font-style:italic;">因为作者不用sass和less，所以不做示例展示</p>
-    <!-- <el-button @click="showWay2">查看示例（sass）</el-button>
-    <el-button @click="showWay3">查看示例（less）</el-button> -->
+    <el-button @click="showWay2">查看示例（css）</el-button>
+    <p>
+      当 lang="sass" 或者 "less" 时，使用 <B>/deep/</B> 或者 <B>::v-deep</B>
+    </p>
+    <p style="font-style:italic;">因为作者不用 sass 和 less ，所以不做示例展示</p>
+
     <way-1 ref="way1" v-if="way1Visible"></way-1>
-    <!-- <way-2 ref="way2" v-if="way2Visible"></way-2>
-    <way-3 ref="way3" v-if="way3Visible"></way-3> -->
+    <way-2 ref="way2" v-if="way2Visible"></way-2>
   </div>
 </template>
 
 <script>
 import Way1 from './ways/penerate-way1.vue'
-// import Way2 from './ways/penerate-way2.vue'
-// import Way3 from './ways/penerate-way3.vue'
+import Way2 from './ways/penerate-way2.vue'
 export default {
   data () {
     return {
       way1Visible: false,
-      // way2Visible: false,
-      // way3Visible: false
+      way2Visible: false
     }
   },
   components: {
     Way1,
-    // Way2,
-    // Way3
+    Way2
   },
   methods: {
     showWay1 () {
@@ -46,18 +45,12 @@ export default {
         this.$refs['way1'].init()
       })
     },
-    // showWay2 () {
-    //   this.way2Visible = true
-    //   this.$nextTick(() => {
-    //     this.$refs['way2'].init()
-    //   })
-    // },
-    // showWay3 () {
-    //   this.way3Visible = true
-    //   this.$nextTick(() => {
-    //     this.$refs['way3'].init()
-    //   })
-    // }
+    showWay2 () {
+      this.way2Visible = true
+      this.$nextTick(() => {
+        this.$refs['way2'].init()
+      })
+    }
   }
 }
 </script>
---
+++ src/view/style-customize/style-penetrate/ways/penerate-way2.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-dialog
+    title="方式2"
+    :visible.sync="dialogVisible"
+    width="30%"
+    :before-close="handleClose">
+    <p>利用样式穿透将选中后显示在方框中的文字换一个颜色</p>
+    <el-select v-model="value" placeholder="请选择" class="selectExample">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+      options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
+    }
+  },
+  methods: {
+    init () {
+      this.dialogVisible = true
+    },
+    handleClose() {
+      this.$confirm('确认关闭？', '提示', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${ action }`
+          })
+        }
+      }).then(() => {
+        this.dialogVisible = false
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /** 将选中后显示在方框中的文字换一个颜色 */
+  .selectExample /deep/ .el-input__inner {
+    color: rgb(17, 223, 85);
+  }
+</style>
